f909e4728a95b8805515fba8069a82e68b568718
Lennix <lennix@mc-wow.de>
2010-05-16 15:17:15 +0200

    Revert "Potential fix for crashes"
    This reverts commit c714862b106363834c6eb6aade328ce6503377d9.

--- src/game/WorldSession.cpp
+++ src/game/WorldSession.cpp
@@ -442,12 +442,9 @@ void WorldSession::LogoutPlayer(bool Save)
         // the player may not be in the world when logging out
         // e.g if he got disconnected during a transfer to another map
         // calls to GetMap in this case may cause crashes
-        /*Map* _map = _player->GetMap();
+        Map* _map = _player->GetMap();
         _map->Remove(_player, true);
-        SetPlayer(NULL); */                                  // deleted in Remove call
-
-		if(_player->IsInWorld()) _player->GetMap()->Remove(_player,true);
-		SetPlayer(NULL);
+        SetPlayer(NULL);                                    // deleted in Remove call
 
         ///- Send the 'logout complete' packet to the client
         WorldPacket data( SMSG_LOGOUT_COMPLETE, 0 );
